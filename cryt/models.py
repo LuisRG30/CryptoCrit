4333556d30d193869850e996ae8515a8e15fea7a
Luis Ramirez Guzman <luisramir@microsoft.com>
2022-04-08 15:29:10 -0500

    Add model for pdf or xml files

--- cryt/models.py
+++ cryt/models.py
@@ -10,3 +10,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.email}"
+
+class Document(models.Model):
+    document = models.FileField()
+    owner = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    shared_with = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='shared', blank=True)
+
+    def __str__(self):
+        return f"{self.document}"
+    
