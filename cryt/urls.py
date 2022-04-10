0386eaa046b2e5a206ea24d7be0c5504ece01900
Luis Ramirez Guzman <luisramir@microsoft.com>
2022-04-10 17:37:20 -0500

    Restricted access to files

--- cryt/urls.py
+++ cryt/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include, re_path
 
 from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    re_path(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], protected_serve, {'document_root': settings.MEDIA_ROOT})
 ]
--- cryt/views.py
+++ cryt/views.py
@@ -1,10 +1,12 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.static import serve
 
 from .forms import SignDocForm
 
-# Create your views here.
+# Index page. Creates and receives sign document form.
 def index(request):
     if request.method == 'POST':
         form = SignDocForm(request.POST, request.FILES)
@@ -20,3 +22,10 @@ def index(request):
         form = SignDocForm()
         return render(request, 'index.html', {'form': form})
         
+
+#Serves documents depending on user credentials.
+#It is pending to select specific user authorization. In the meantime, user has to be logged in.
+@login_required
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root, show_indexes)
+
