ab77cbc6c1405633282f26d08bc62a6edce6bcd8
Luis Ramirez Guzman <luisramir@microsoft.com>
2022-04-24 10:24:18 -0500

    Add storages to installed apps

--- CryptoCrit/settings.py
+++ CryptoCrit/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google'
+    'allauth.socialaccount.providers.google',
+    'storages'
 ]
 
 
